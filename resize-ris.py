3a02e02576ba64d7b3ec7c614a02a4db49a85107
zvezdochiot <mykaralw@yandex.ru>
2021-03-13 19:18:39 +0300

    0.20210313: simplify layers

--- resize-ris.py
+++ resize-ris.py
@@ -9,46 +9,50 @@ from gimpfu import *
 PROC_NAME = 'python-fu-resize-ris'
 PROC_LABEL = 'Resize RIS'
 PROC_AUTHOR = 'zvezdochiot <zvezdochiot@sourceforge.net>'
-PROC_DATE = '2021-03-10'
+PROC_DATE = '2021-03-13'
 
 def ris(image, drawable, width, height, interpol):
     image.undo_group_start()
     orig_width = pdb.gimp_image_width(image)
     orig_height = pdb.gimp_image_height(image)
 
-    copy_image = pdb.gimp_image_duplicate(image)
-    copy_drawable = pdb.gimp_image_get_active_drawable(copy_image)
-    blur_image = pdb.gimp_image_duplicate(image)
-    blur_drawable = pdb.gimp_image_get_active_drawable(blur_image)
+    image_copy = pdb.gimp_image_duplicate(image)
+    drawable_copy = pdb.gimp_image_get_active_drawable(image_copy)
+    image_blur = pdb.gimp_image_duplicate(image)
+    drawable_blur = pdb.gimp_image_get_active_drawable(image_blur)
 
     pdb.gimp_context_set_interpolation(interpol)
-    pdb.gimp_image_scale(blur_image, width, height)
-    pdb.gimp_image_scale(blur_image, orig_width, orig_height)
+    pdb.gimp_image_scale(image_blur, width, height)
+    pdb.gimp_image_scale(image_blur, orig_width, orig_height)
 
-    blur_layer = pdb.gimp_layer_new_from_visible(blur_image, image, "ResizeBlur")
-    pdb.gimp_image_insert_layer(image, blur_layer, None, -1)
-    pdb.gimp_image_set_active_layer(image, blur_layer)
-    blur_layer.mode = SUBTRACT_MODE
+    layer_blur = pdb.gimp_layer_new_from_visible(image_blur, image, "ResizeBlur")
+    pdb.gimp_image_insert_layer(image, layer_blur, None, -1)
+    pdb.gimp_image_set_active_layer(image, layer_blur)
 
-    copy_layer = pdb.gimp_layer_new_from_visible(copy_image, image, "CopyOrig")
-    pdb.gimp_image_insert_layer(image, copy_layer, None, -1)
-    pdb.gimp_image_set_active_layer(image, copy_layer)
-    copy_layer.mode = ADDITION_MODE
+    layer_copy = pdb.gimp_layer_new_from_visible(image_copy, image, "CopyOrig")
+    pdb.gimp_image_insert_layer(image, layer_copy, None, -1)
+    pdb.gimp_image_set_active_layer(image, layer_copy)
+    layer_copy.mode = SUBTRACT_MODE
 
-    blur_layer2 = pdb.gimp_layer_new_from_visible(blur_image, image, "ResizeBlur2")
-    pdb.gimp_image_insert_layer(image, blur_layer2, None, -1)
-    pdb.gimp_image_set_active_layer(image, blur_layer2)
-    blur_layer2.mode = SUBTRACT_MODE
+    layer_sub = pdb.gimp_image_merge_down(image, layer_copy, 0)
+    layer_sub.name = "RIS_SUBTRACT"
+    layer_sub.mode = SUBTRACT_MODE
 
-    copy_layer2 = pdb.gimp_layer_new_from_visible(copy_image, image, "CopyOrig2")
-    pdb.gimp_image_insert_layer(image, copy_layer2, None, -1)
-    pdb.gimp_image_set_active_layer(image, copy_layer2)
-    copy_layer2.mode = ADDITION_MODE
+    layer_copy = pdb.gimp_layer_new_from_visible(image_copy, image, "CopyOrig")
+    pdb.gimp_image_insert_layer(image, layer_copy, None, -1)
+    pdb.gimp_image_set_active_layer(image, layer_copy)
 
-    #pdb.gimp_image_merge_down(image, blur_layer, 0)
+    layer_blur = pdb.gimp_layer_new_from_visible(image_blur, image, "ResizeBlur")
+    pdb.gimp_image_insert_layer(image, layer_blur, None, -1)
+    pdb.gimp_image_set_active_layer(image, layer_blur)
+    layer_blur.mode = SUBTRACT_MODE
 
-    pdb.gimp_image_delete(copy_image)
-    pdb.gimp_image_delete(blur_image)
+    layer_plus = pdb.gimp_image_merge_down(image, layer_blur, 0)
+    layer_plus.name = "RIS_ADDITION"
+    layer_plus.mode = ADDITION_MODE
+
+    pdb.gimp_image_delete(image_copy)
+    pdb.gimp_image_delete(image_blur)
 
     pdb.gimp_image_scale(image, width, height)
 
